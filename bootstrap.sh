b68e38b9d14fca423e07d9a63d324525a1b736ff
Saravana Periyasamy <saravanakumar.periyasamy@gmail.com>
2022-04-28 20:44:39 -0500

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install ansible
+brew install ansible
+
+# invoke ansible
+sudo ansible-pull -U https://github.com/iblogdev/workstation-setup.git
