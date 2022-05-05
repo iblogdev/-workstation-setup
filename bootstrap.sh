5b8215cec03849e8f37bb4f2f3f48b5b3b81f678
Elizabeth Thomas <email2eliza@gmail.com>
2022-05-04 22:51:42 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/"$(whoami)"/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install git
 brew install git
